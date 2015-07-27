e2adddeadfc3ebcd2a86bf0f26e79e77bcf38dc4
Flax163 <goncharov.valia@yandex.ru>
2015-07-27 22:50:14 +0400

    add post query from get timetable

--- timetable/ViewController.swift
+++ timetable/ViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    let tolgasTimeTableUrl:String = "http://www.tolgas.ru/services/raspisanie/"
 
-    override func viewDidLoad() {
+    @IBAction func responeButton(sender: UIButton)
+    {
+        var url:NSURL = NSURL(string: tolgasTimeTableUrl)!
+        var request2:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        request2.HTTPMethod = "POST"
+        var bodyData:String = "rel=0&grp=177&prep=0&audi=0&vr=576&from=30.04.2015&to=30.06.2015&submit_button=ПОКАЗАТЬ";
+        request2.HTTPBody = bodyData.dataUsingEncoding(NSWindowsCP1252StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request2, completionHandler:
+            {
+                data, response, error in
+                if error != nil
+                {
+                    println("error=\(error)")
+                    return
+                }
+                
+                println("response = \(response)")
+                
+                var responseString:String = NSString(data: data, encoding: NSWindowsCP1252StringEncoding)! as String
+                println("responseString = \(responseString)")
+            })
+        
+        task.resume()
+    }
+ 
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
